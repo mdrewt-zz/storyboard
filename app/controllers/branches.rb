a6aa23f1e5fdb4fd1e9c043bc43e95a6a40d515f
Drew Teter <mteter13@gmail.com>
2014-06-27 15:09:08 -0400

    added routes for branches

--- app/controllers/branches.rb
+++ app/controllers/branches.rb
@@ -1,8 +1,18 @@
 get '/branch/:id' do
+  @segment = Segment.find(params[:id])
+  erb :segment
 end
 
 get '/branch/:id/edit' do
+  @segment = Segment.find(params[:id])
+  if session[:user].id == @segment.user.id
+    erb :edit_segment
+  else
+    erb :permission_denied
+  end
 end
 
-put '/branch/:id' do
+put '/branch/:id/edit' do
+  segment = Segment.find(params[:id]).update(params(:edit))
+  redirect "/branch/#{segment.id}"
 end
