8604e8eba5c768bdca96860f970a7e691f4b7502
Joe & Stella Kim <pair+jdcohen9.stellajkimm@devbootcamp.com>
2014-06-27 16:32:35 -0400

    added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
   # Remember to create a migration!
   validates :name, presence: true
   validates :email, uniqueness: true
   has_many :stories
   has_many :segments
 
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 
   def self.authenticate(params)
     email = params[:email]
