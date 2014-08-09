32f21f4f9265a75db098b183be94081e6ed98e88
Drew Teter <mteter13@gmail.com>
2014-08-09 17:24:23 -0400

    json route now returns a json object

--- app/controllers/json_test.rb
+++ app/controllers/json_test.rb
@@ -2,7 +2,6 @@ get '/json/:parent_id/:index' do
   @segment = Segment.where(parent_id: params[:parent_id])
   if @segment.length > 0
     @segment = @segment[params[:index].to_i % @segment.length]
-    # erb :testing
     @segment.to_json
   else
     # return not found
