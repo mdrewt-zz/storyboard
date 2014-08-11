9c645b267724222e7d4826925f1cfcc3fe5541e3
Drew Teter <mteter13@gmail.com>
2014-08-11 09:27:14 -0400

    json controller returns #descended_from

--- app/controllers/json_test.rb
+++ app/controllers/json_test.rb
@@ -1,10 +1,5 @@
 get '/json/:parent_id/:index' do
-  @segment = Segment.where(parent_id: params[:parent_id])
-  if @segment.length > 0
-    @segment = @segment[params[:index].to_i % @segment.length]
-    @segment.to_json
-  else
-    # return not found
-  end
+  @segments = Segment.descended_from(params[:parent_id], params[:index])
+  @segments.to_json
 end
 
