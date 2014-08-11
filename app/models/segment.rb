bf9539e96158289d3bfa81d4ada03c8614a1f430
Drew Teter <mteter13@gmail.com>
2014-08-11 09:28:11 -0400

    minor bug fix

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -4,7 +4,8 @@ class Segment < ActiveRecord::Base
   belongs_to :parent, class_name: "Segment"
 
   def self.descended_from(parent_id, index = 0)
-    seg = Segment.where(parent_id: parent_id)[index]
+    seg = Segment.where(parent_id: parent_id)
+    seg = seg[index % seg.length]
     children = seg.branches
     children.length > 0 ? [seg] << Segment.descended_from(seg.id) : seg
   end
