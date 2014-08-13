679671d60563a752a5ed681f5292492d20cf0267
Drew Teter <mteter13@gmail.com>
2014-08-13 16:45:23 -0400

    misc fixes

--- app/controllers/json_test.rb
+++ app/controllers/json_test.rb
@@ -1,7 +1,7 @@
 get '/json/:story_id/:parent_id/:index' do
-  @segment = Segment.where("story_id = ? and parent_id = ? and index = ?", params[:story_id], params[:parent_id], params[:index])
+  @segment = Segment.where("story_id = ? and parent_id = ? and index = ?", params[:story_id], params[:parent_id], params[:index]).first
   @parents = @segment.ancestors
   @children = @segment.descendants
-  (@parents + [@segment] + @children).to_json
+  (@parents + @children).uniq.to_json
 end
 
