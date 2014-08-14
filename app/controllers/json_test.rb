25d46a5b4e1e3447db0e0c5c6db72299bf0961e6
Drew Teter <mteter13@gmail.com>
2014-08-13 21:07:32 -0400

    proc file added

--- app/controllers/json_test.rb
+++ app/controllers/json_test.rb
@@ -1,5 +1,5 @@
 get '/json/:story_id/:parent_id/:index' do
-  @segments = Segment.where("story_id = ? and parent_id = ?", params[:story_id], params[:parent_id])
+  @segments = Segment.where(story_id: params[:story_id], parent_id: params[:parent_id])
   @segment = @segments.select{|seg| seg.index == params[:index].to_i % @segments.length}.first
   @parents = @segment.ancestors.sort_by{|seg| seg.id}
   @children = @segment.descendants.sort_by{|seg| seg.id}
